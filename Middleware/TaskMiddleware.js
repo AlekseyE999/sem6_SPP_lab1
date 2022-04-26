a812eef0da58aee5e1cb40af6f31538e51559bde
AlekseyE <yury.e74@gmail.com>
2022-04-26 16:53:08 +0300

    fix adding files

--- Middleware/TaskMiddleware.js
+++ Middleware/TaskMiddleware.js
@@ -10,8 +10,8 @@ exports.addTask = function (req, res, next) {
     const subject = req.body.subject;
     const number = req.body.number;
     const status = req.body.status;
-    const deadline = req.body.deadline;
-    const attach = req.file.originalname;
+    const deadline = req.body.deadline;  
+    const attach = req.file?.originalname;
     const task = new Task(subject, number, status, deadline, attach);
     task.save();
     res.redirect("/");
